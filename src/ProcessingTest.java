0a95e7692a03aa2bab2ab518f553c963b5a6bbd6
Map1eUM <chenr5@myumanitoba.ca>
2022-01-25 14:08:12 -0600

    The COMP1020 repo established. All files written in Java as course requirement. Fight toward A+!

---
+++ src/ProcessingTest.java
@@ -0,0 +1,18 @@
+
+import processing.core.PApplet;
+
+public class ProcessingTest extends PApplet {
+
+    public void settings(){
+        size(200, 200);
+    }
+
+    public void draw(){
+        background(0);
+        ellipse(mouseX, mouseY, 20, 20);
+    }
+
+    public static void main(String... args){
+        PApplet.main("ProcessingTest");
+    }
+}
