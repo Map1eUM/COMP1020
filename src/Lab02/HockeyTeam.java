3435bd190321bc386965a80c7c1e773a85d5e891
Map1eUM <chenr5@myumanitoba.ca>
2022-02-10 22:58:11 -0600

    COMP lab2 silver, instruction details are waiting for confirmation

--- src/Lab02/HockeyTeam.java
+++ src/Lab02/HockeyTeam.java
@@ -1,14 +1,25 @@
 package Lab02;
 
+
 public class HockeyTeam {
     private String name;
     private int win, loss, otLoss;
+    public static int ct = 0;
+    public static int N = 100;
+    private static HockeyTeam[] lst = new HockeyTeam[N];
 
     public HockeyTeam(String name, int win, int loss, int otLoss) {
         this.name = name;
         this.win = win;
         this.loss = loss;
         this.otLoss = otLoss;
+        this.lst[ct++] = this;
+    }
+
+    public static void listTeams() {
+        for (int i = 0; i < ct; ++i) {
+            System.out.println(toString(i));
+        }
     }
 
     public void won() {
@@ -23,11 +34,14 @@ public class HockeyTeam {
         ++this.otLoss;
     }
 
-    public int point() {
-        return this.win * 2 + this.otLoss;
+    public static int point(int k) {
+        return lst[k].win * 2 + lst[k].otLoss;
     }
 
-    public String toString() {
-        return this.name + "(" + this.win + "," + this.loss + "," + this.otLoss + "=" + point() + ")";
+    public static String toString(int k) {
+        return lst[k].name + "(" + lst[k].win + "," + lst[k].loss + "," + lst[k].otLoss + "=" + point(k) + ")";
     }
 }
+
+
+
