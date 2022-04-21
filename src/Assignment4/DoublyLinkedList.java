436c5902d33068f2ab2b7e81dc1764868b4a6e7d
Map1eUM <chenr5@myumanitoba.ca>
2022-04-21 16:03:31 -0500

    A4P1 finished without testing

--- src/Assignment4/DoublyLinkedList.java
+++ src/Assignment4/DoublyLinkedList.java
@@ -1,41 +1,61 @@
 package Assignment4;
 
-public class DoublyLinkedList
-{
+public class DoublyLinkedList {
 	/*
 	The following code is provided. You are not allowed to modify it.
 	*/
-	
-	//
-	//Instance variables
-	//
-	private Node first;  //the first LinkedList.Node (or the top LinkedList.Node) of the list
-	private Node last;  //the last LinkedList.Node of the list
-	
-	//
-	//Instance methods
-	//
-	
-	//accessor for the first LinkedList.Node
-	public Node getFirst()
-	{
-		return first;
-	}
-	
-	//accessor for the last LinkedList.Node
-	public Node getLast()
-	{
-		return last;
-	}
+
+    //
+    //Instance variables
+    //
+    private Node first;  //the first LinkedList.Node (or the top LinkedList.Node) of the list
+    private Node last;  //the last LinkedList.Node of the list
+
+    //
+    //Instance methods
+    //
+
+    //accessor for the first LinkedList.Node
+    public Node getFirst() {
+        return first;
+    }
+
+    //accessor for the last LinkedList.Node
+    public Node getLast() {
+        return last;
+    }
 	
 	/*
 	End of the provided code.
 	*/
-	
-	//Add your code below.
 
-	public void addFront(Number data) {
+    //Add your code below.
 
-	}
+    public void addFront(Number data) {
+        this.first = new Node(data, null, this.first);
+    }
+
+    public void addEnd(Number data) {
+        this.last = new Node(data, this.last, null);
+    }
+
+    public int size() {
+        return sizeRec(this.first);
+    }
+
+    private int sizeRec(Node node) {
+        if (node == null) return 0;
+        return sizeRec(node.getNext()) + 1;
+    }
+
+    private String toStringReversedRec(Node node) {
+        if (node == null) return " >>";
+        return String.valueOf(node.getData()) + toStringReversedRec(node.getNext());
+    }
+
+    public String toStringReversed() {
+        return "<< " + toStringReversedRec(this.last);
+    }
+
 
 }
